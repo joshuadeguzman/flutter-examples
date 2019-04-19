b68413bb0339eb6a840aac0de7fb18bb0b667e3c
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2019-04-19 23:16:08 +0800

    Add setup for home screen

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,19 @@
+// Copyright 2019 Joshua de Guzman (https://jmdg.io). All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter MovieHub Demo'),
+      ),
+      body: Center(
+        child: Text('Demo'),
+      ),
+    );
+  }
+}
