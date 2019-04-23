7e8852442175e65582b7309a3b8d74388b05bccf
jdeguzman <devjoshuadeguzman@gmail.com>
2019-04-23 17:02:20 +0800

    Clean up banner view

--- lib/widgets/views/showcase_banner_view.dart
+++ lib/widgets/views/showcase_banner_view.dart
@@ -4,62 +4,78 @@
 
 import 'package:flutter/material.dart';
 
+Widget _buildBackdropImageView(double height, url) {
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.transparent,
+      image: DecorationImage(
+          fit: BoxFit.cover,
+          // https://stackoverflow.com/questions/50713888/how-can-i-show-image-from-network-in-flutter-boxdecoration/50714191
+          image: NetworkImage(url)),
+    ),
+    height: height,
+  );
+}
+
+Widget _buildBackdropTopGradientOverlayView(double height) {
+  return Container(
+    height: height,
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: FractionalOffset.topCenter,
+        end: FractionalOffset.bottomCenter,
+        colors: [
+          Colors.black.withOpacity(0.0),
+          Colors.black,
+        ],
+        stops: [0.0, 1],
+      ),
+    ),
+  );
+}
+
+Widget _buildBackdropBottomGradientOverlayView(double height) {
+  return Container(
+    height: height,
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: FractionalOffset.bottomCenter,
+        end: FractionalOffset.topCenter,
+        colors: [
+          Colors.black.withOpacity(0.0),
+          Colors.black,
+        ],
+        stops: [0.0, 1.0],
+      ),
+    ),
+  );
+}
+
+Widget _buildCarouselView(double height, Widget showcaseWidget) {
+  return Container(
+    height: height,
+    // With respect to the appbar gradient effect
+    padding: EdgeInsets.only(top: 20),
+    child: Column(
+      // https://stackoverflow.com/questions/51545768/flutter-vertically-center-column
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[showcaseWidget],
+    ),
+  );
+}
+
 Widget buildShowcaseBannerView(
     BuildContext context, String url, Widget showcaseWidget) {
   var rootWidth = MediaQuery.of(context).size.width;
-  var containerHeight = rootWidth / 1.15;
+  var height = rootWidth / 1.15;
   return Container(
     child: Stack(
       children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.transparent,
-            image: DecorationImage(
-                fit: BoxFit.cover,
-                // https://stackoverflow.com/questions/50713888/how-can-i-show-image-from-network-in-flutter-boxdecoration/50714191
-                image: NetworkImage(url)),
-          ),
-          height: containerHeight,
-        ),
-        Container(
-          height: containerHeight,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: FractionalOffset.topCenter,
-              end: FractionalOffset.bottomCenter,
-              colors: [
-                Colors.black.withOpacity(0.0),
-                Colors.black,
-              ],
-              stops: [0.0, 1],
-            ),
-          ),
-        ),
-        Container(
-          height: containerHeight,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: FractionalOffset.bottomCenter,
-              end: FractionalOffset.topCenter,
-              colors: [
-                Colors.black.withOpacity(0.0),
-                Colors.black,
-              ],
-              stops: [0.0, 1.0],
-            ),
-          ),
-        ),
-        Container(
-          height: containerHeight,
-          // With respect to the appbar gradient effect
-          padding: EdgeInsets.only(top: 20),
-          child: Column(
-            // https://stackoverflow.com/questions/51545768/flutter-vertically-center-column
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[showcaseWidget],
-          ),
-        )
+        _buildBackdropImageView(height, url),
+        _buildBackdropTopGradientOverlayView(height),
+        _buildBackdropBottomGradientOverlayView(height),
+        _buildCarouselView(height, showcaseWidget),
       ],
     ),
   );
