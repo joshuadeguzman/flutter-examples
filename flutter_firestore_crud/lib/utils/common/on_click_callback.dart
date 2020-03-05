943253bfd4ff7a5c24c9cd0950f29002f816a4b1
Joshua de Guzman <joshua.maiquez.deguzman@gmail.com>
2020-03-06 00:45:37 +0800

    Clean up sample branch

--- flutter_firestore_crud/lib/utils/common/on_click_callback.dart
+++
@@ -1,5 +0,0 @@
-// Copyright 2020 Joshua de Guzman (https://joshuadeguzman.github.io). All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-typedef void OnClickCallback<T>(T item);
--- flutter_moviehub/lib/constants/moviedb.dart
+++
@@ -1,9 +0,0 @@
-// Copyright 2019 Joshua de Guzman (https://jmdg.io). All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-class MovieType {
-  static const String UPCOMING = 'upcoming';
-  static const String POPULAR = 'popular';
-  static const String TOP_RATED = 'top_rated';
-}
