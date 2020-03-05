7ebd7dc8b0bc64ee897aca7b11ddbe9bc81f7367
Joshua de Guzman <joshua.maiquez.deguzman@gmail.com>
2020-03-06 00:44:41 +0800

    Clean up sample branch

--- flutter_moviehub/lib/model/base_model.dart
+++
@@ -1,5 +0,0 @@
-// Copyright 2019 Joshua de Guzman (https://jmdg.io). All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-abstract class BaseModel {}
